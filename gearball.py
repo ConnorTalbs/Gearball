4aa780be499cd0352d86d8a4e26db9da237c6046
ConnorTalbs <69060736+ConnorTalbs@users.noreply.github.com>
2022-09-06 20:21:46 -0400

    Add files via upload
    Rewrote the turning, and implemented a random move feature

--- gearball.py
+++ gearball.py
@@ -3,25 +3,25 @@
 #8/30/2022
 #Build a program that displays and randomizes a gearball
 #########################################################
-
+import random
 from operator import mod
 from turtle import right
 
 ball = [[['r'],['r','r','r'],['r'],['r','r','r'],['r'],['r','r','r'],['r']],
         [['y'],['y','y','y'],['y'],['y','y','y'],['y'],['y','y','y'],['y']],
+        [['p'],['p','p','p'],['p'],['p','p','p'],['p'],['p','p','p'],['p']],
         [['o'],['o','o','o'],['o'],['o','o','o'],['o'],['o','o','o'],['o']],
-        [['w'],['w','w','w'],['w'],['w','w','w'],['w'],['w','w','w'],['w']],
         [['b'],['b','b','b'],['b'],['b','b','b'],['b'],['b','b','b'],['b']],
         [['g'],['g','g','g'],['g'],['g','g','g'],['g'],['g','g','g'],['g']]]
 
 titles = ['front','right','back','left','top','bottom']
 
 # r - front
-# g - right
-# b - back
-# y - left
-# o - top
-# p - bottom
+# y - right
+# o - back
+# w - left
+# b - top
+# g - bottom
 
 def printBall():
     for i in range(6):
@@ -33,6 +33,7 @@ def printBall():
         print("          ",ball[i][6],"\n")
 
 def topClockWise():
+    '''
     temp = ['','','']
     temp1 = ['','','']
     temp2 = ['','','']
@@ -43,14 +44,15 @@ def topClockWise():
         temp3[i] = ball[1][1][i]
     for i in range(4):
         for j in range(3):
-            temp[j] = ball[i][1][j]
-            ball[i][1][j] = ball[i+2][1][j]
-            ball[i+2][1][j] = temp[j]
+            if i < 2:
+                temp[j] = ball[i][1][j]
+                ball[i][1][j] = ball[i+1][1][j]
+                ball[i+1][1][j] = temp[j]
             if i == 2:
                 ball[i][1][j] = temp2[j]
             if i == 3:
                 ball[i][1][j] = temp3[j]
-    '''
+    
     for i in range(3):
         temp4[i] = ball[0][3][j]
     for j in range(4):
@@ -61,24 +63,61 @@ def topClockWise():
                 ball[j+1][3][x] = temp4[x]
             else:
                 ball[j][3][x] = temp4[x] 
-'''
+    
+    #rotate90degrees()
+    '''
+    temp = ['','','']
+    for i in range(3):
+        temp[i] = ball[0][1][i]
+    for i in range(4):
+        for j in range(3):
+            ball[i][1][j] = ball[i+1][1][j]
+            if i == 3:
+                ball[i][1][j] = temp[j]
+    #rotate90Clockwise()
+
 def topCounterCW():
+    '''
     temp = ['','','']
     temp1 = ['','','']
     temp2 = ['','','']
     temp3 = ['','','']
+    temp4 = ['','','']
     for i in range(3):
         temp2[i] = ball[2][1][i]
         temp3[i] = ball[3][1][i]
     for i in reversed(range(4)):
         for j in reversed(range(3)):
-            temp[j] = ball[i][1][j]
-            ball[i][1][j] = ball[i-2][1][j]
-            ball[i-2][1][j] = temp[j]
+            if i > 1:
+                temp[j] = ball[i][1][j]
+                ball[i][1][j] = ball[i-2][1][j]
+                ball[i-2][1][j] = temp[j]
             if i == 1:
-                ball[i][1][j] = temp2[j]
-            if i == 0:
                 ball[i][1][j] = temp3[j]
+            if i == 0:
+                ball[i][1][j] = temp2[j]
+    
+    #moving middle
+    for i in range(3):
+        temp4[i] = ball[3][3][j]
+    for j in reversed(range(4)):
+        for x in range(3):
+            if j !=0:
+                temp4[x] = ball[j][3][x]
+                ball[j][3][x] = ball[j-1][3][x]
+                ball[j-1][3][x] = temp4[x]
+            else:
+                ball[j][3][x] = temp4[x] 
+                '''
+    temp = ['','','']
+    for i in range(3):
+        temp[i] = ball[3][1][i]
+    for i in reversed(range(4)):
+        for j in range(3):
+            ball[i][1][j] = ball[i-1][1][j]
+            if i == 0:
+                ball[i][1][j] = temp[j]         
+                
 
 def bottomClockWise():
     temp = ['','','']
@@ -91,11 +130,31 @@ def bottomClockWise():
                 ball[i][5][j] = temp[j]
 
 def bottomCounterCW():
+    '''
+    temp = ['','','']
+    temp1 = ['','','']
+    temp2 = ['','','']
+    temp3 = ['','','']
+    temp4 = ['','','']
+    for i in range(3):
+        temp2[i] = ball[2][5][i]
+        temp3[i] = ball[3][5][i]
+    for i in reversed(range(4)):
+        for j in reversed(range(3)):
+            if i > 1:
+                temp[j] = ball[i][5][j]
+                ball[i][5][j] = ball[i-1][5][j]
+                ball[i-1][5][j] = temp[j]
+            if i == 1:
+                ball[i][5][j] = temp3[j]
+            if i == 0:
+                ball[i][5][j] = temp2[j]
+                '''
     temp = ['','','']
     for i in range(3):
         temp[i] = ball[3][5][i]
     for i in reversed(range(4)):
-        for j in reversed(range(3)):
+        for j in range(3):
             ball[i][5][j] = ball[i-1][5][j]
             if i == 0:
                 ball[i][5][j] = temp[j]
@@ -156,7 +215,7 @@ def leftClockWise():
                     ball[0][j+1][0] = temp1[x]
                     ball[5][j+1][0] = temp2[x]
 
-def leftCounterCW(count):
+def leftCounterCW():
     temp1 = ['','','']
     temp2 = ['','','']
     temp3 = ['','','']
@@ -173,17 +232,52 @@ def leftCounterCW(count):
                     ball[2][j-1][0] = temp1[x]
                     ball[0][j-1][0] = temp3[x]
                     ball[5][j-1][0] = temp2[x]
-    if count % 3 == 0:
-        for j in range(0,8,2):
-            if ball[0][j][0] == 'r':
-                ball[0][j][0] = 'b'
-            else:
-                ball[0][j][0] = 'r'
 
-#def rotate90():
+def rotate90Clockwise():
+    A = [['','',''],['','',''],['','','']]
+    for i in range(3):
+        for j,x in zip(range(2,8,2),range(3)):
+            A[i][x] = ball[4][j-1][x]
+    N = len(A[0])
+    for i in range(N // 2):
+        for j in range(i, N - i - 1):
+            temp = A[i][j]
+            A[i][j] = A[N - 1 - j][i]
+            A[N - 1 - j][i] = A[N - 1 - i][N - 1 - j]
+            A[N - 1 - i][N - 1 - j] = A[j][N - 1 - i]
+            A[j][N - 1 - i] = temp
+    for i in range(3):
+        for j,x in zip(range(2,8,2),range(3)):
+            ball[4][j-1][x] = A[i][x]
+    print(A)
 
+def topCW():
+    topClockWise()
+    bottomCounterCW()
+def topCCW():
+    topCounterCW()
+    bottomClockWise()
+def botCW():
+    bottomClockWise()
+    topCounterCW()
+def botCCW():
+    bottomCounterCW()
+    topClockWise()
+def rightCW():
+    rightClockWise()
+    leftCounterCW()
+def rightCCW():
+    rightCounterCW()
+    leftClockWise()
+def leftCW():
+    leftClockWise()
+    rightCounterCW()
+def leftCCW():
+    leftCounterCW()
+    rightClockWise()
 
 def main():
+    '''
     count = 1
     while(True):
         printBall()
@@ -196,6 +290,12 @@ def main():
             topCounterCW()
             bottomClockWise()
             count += 1
+        if face == 'b' and direc == 'cw':
+            bottomClockWise()
+            topCounterCW()
+        if face == 'b' and direc == 'ccw':
+            bottomCounterCW()
+            topClockWise
         if face == 'r' and direc == 'cw':
             rightClockWise()
             leftCounterCW()
@@ -209,12 +309,21 @@ def main():
             rightCounterCW()
             count += 1
         if face == 'l' and direc == 'ccw':
-            leftCounterCW(count)
+            leftCounterCW()
             rightClockWise()
             count += 1
     #topRightTurn()
     #topLeftTurn()
     #bottomRightTurn()
     #bottomLeftTurn()
+    '''
+
+    list = [topCW(),topCCW(),botCW(),botCCW(),rightCW(),rightCCW(),leftCW(),leftCCW()]
+    choice = input("How many moves would you like to make: ")
+    choice = int(choice)
+    while choice != 0:
+        random.choice(list)
+        choice -= 1
+    printBall()
 
 main()
